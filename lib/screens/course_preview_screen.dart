f557d1837b312972f1034b76a8279085c73879a5
Aryansh Gupta <aryanshgupta@protonmail.com>
2023-03-15 17:42:56 +0530

    removed value listener

--- lib/screens/course_preview_screen.dart
+++ lib/screens/course_preview_screen.dart
@@ -16,141 +16,138 @@ class CoursePreviewScreen extends StatefulWidget {
 }
 
 class _CoursePreviewScreenState extends State<CoursePreviewScreen> {
+  var authInfoBox = Hive.box('auth_info');
+
   bool isLoading = false;
 
   bool isEnrolled = false;
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: Hive.box('auth_info').listenable(),
-      builder: (context, authInfoBox, child) {
-        return Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.arrow_back_ios_new_rounded,
-                color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
-              ),
-              splashRadius: 25.0,
-            ),
-            title: Text(
-              "Details",
-              style: TextStyle(
-                color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
-              ),
-            ),
-            centerTitle: true,
-            elevation: 0,
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
           ),
-          body: SafeArea(
-            child: ListView(
-              children: [
-                Image.network(
-                  "https://i.ytimg.com/vi/${widget.course.link.substring(30, 41)}/maxresdefault.jpg",
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(height: 10.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Text(
-                    widget.course.title,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Text(
-                    widget.course.channelName,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Theme.of(context).brightness == Brightness.dark ? Colors.white54 : Colors.black45,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 15.0),
-              ],
-            ),
+          splashRadius: 25.0,
+        ),
+        title: Text(
+          "Details",
+          style: TextStyle(
+            color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
           ),
-          floatingActionButton: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 50.0,
-              width: MediaQuery.of(context).size.width,
-              child: TextButton(
-                onPressed: () {
-                  if (authInfoBox.get("authToken").isEmpty) {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const SigninScreen(),
-                      ),
-                    );
-                  } else {
-                    if (!isLoading) {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      LoadingDialog(context);
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Image.network(
+              "https://i.ytimg.com/vi/${widget.course.link.substring(30, 41)}/maxresdefault.jpg",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Text(
+                widget.course.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Text(
+                widget.course.channelName,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Theme.of(context).brightness == Brightness.dark ? Colors.white54 : Colors.black45,
+                ),
+              ),
+            ),
+            const SizedBox(height: 15.0),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          height: 50.0,
+          width: MediaQuery.of(context).size.width,
+          child: TextButton(
+            onPressed: () {
+              if (authInfoBox.get("authToken").isEmpty) {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SigninScreen(),
+                  ),
+                );
+              } else {
+                if (!isLoading) {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  LoadingDialog(context);
 
-                      var myCoursesBox = Hive.box('my_courses');
-                      Map<dynamic, dynamic> courseList = myCoursesBox.get('courseList') ?? {};
+                  var myCoursesBox = Hive.box('my_courses');
+                  Map<dynamic, dynamic> courseList = myCoursesBox.get('courseList') ?? {};
 
-                      ApiService()
-                          .addCourse(
-                        id: widget.course.id,
-                        token: authInfoBox.get("authToken"),
-                      )
-                          .then((value) {
-                        setState(() {
-                          isLoading = false;
-                        });
-                        Navigator.pop(context);
-                        if (value["success"] == "Course Add Successfully To My Course" || value["error"] == "Already Add To My Course") {
-                          courseList[widget.course.id] = 0.0;
+                  ApiService()
+                      .addCourse(
+                    id: widget.course.id,
+                    token: authInfoBox.get("authToken"),
+                  )
+                      .then((value) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                    Navigator.pop(context);
+                    if (value["success"] == "Course Add Successfully To My Course" || value["error"] == "Already Add To My Course") {
+                      courseList[widget.course.id] = 0.0;
 
-                          myCoursesBox.put("courseList", courseList);
+                      myCoursesBox.put("courseList", courseList);
 
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (context) => CourseScreen(
-                                course: widget.course,
-                                currentUserRating: courseList[widget.course.id],
-                              ),
-                            ),
-                          );
-                        }
-                      }).onError((error, stackTrace) {
-                        setState(() {
-                          isLoading = false;
-                        });
-                        Navigator.pop(context);
-                      });
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => CourseScreen(
+                            course: widget.course,
+                            currentUserRating: courseList[widget.course.id],
+                          ),
+                        ),
+                      );
                     }
-                  }
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(primaryColor),
-                ),
-                child: Text(
-                  isEnrolled ? "Unenroll" : "Enroll",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  ),
-                ),
+                  }).onError((error, stackTrace) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                    Navigator.pop(context);
+                  });
+                }
+              }
+            },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(primaryColor),
+            ),
+            child: Text(
+              isEnrolled ? "Unenroll" : "Enroll",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
               ),
             ),
           ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        );
-      },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
