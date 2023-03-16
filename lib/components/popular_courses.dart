87a0c3449983710f069b192fd50f8d9d069f7d1e
Aryansh Gupta <aryanshgupta@protonmail.com>
2023-03-16 13:25:53 +0530

    added on tap on popular courses

--- lib/components/popular_courses.dart
+++ lib/components/popular_courses.dart
@@ -2,6 +2,9 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:tutopedia/components/shimmer_box.dart';
+import 'package:tutopedia/constants/hive_boxes.dart';
+import 'package:tutopedia/screens/course_preview_screen.dart';
+import 'package:tutopedia/screens/course_screen.dart';
 import 'package:tutopedia/services/api_service.dart';
 
 class PopularCourses extends StatefulWidget {
@@ -28,37 +31,67 @@ class _PopularCoursesState extends State<PopularCourses> {
             ),
             itemCount: snapshot.data!.length,
             itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
-              return Padding(
-                padding: const EdgeInsets.all(7.5),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                  child: CachedNetworkImage(
-                    imageUrl: "https://i.ytimg.com/vi/${snapshot.data![itemIndex].link.substring(30, 41)}/maxresdefault.jpg",
-                    placeholder: (context, url) {
-                      return const ShimmerBox(
-                        height: double.infinity,
-                        width: double.infinity,
-                        borderRadius: 10.0,
-                        margin: 0.0,
-                      );
-                    },
-                    errorWidget: (context, url, error) {
-                      return Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(10.0),
+              return InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+                onTap: () {
+                  Map<dynamic, dynamic> courseList = myCoursesBox.get('courseList') ?? {};
+                  bool isEnrolled = false;
+                  if (courseList.isNotEmpty) {
+                    isEnrolled = courseList.containsKey(snapshot.data![itemIndex].id);
+                  }
+                  if (isEnrolled) {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => CourseScreen(
+                          course: snapshot.data![itemIndex],
+                          currentUserRating: courseList[snapshot.data![itemIndex].id],
+                        ),
+                      ),
+                    );
+                  } else {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => CoursePreviewScreen(
+                          course: snapshot.data![itemIndex],
+                        ),
+                      ),
+                    );
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(7.5),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                    child: CachedNetworkImage(
+                      imageUrl: "https://i.ytimg.com/vi/${snapshot.data![itemIndex].link.substring(30, 41)}/maxresdefault.jpg",
+                      placeholder: (context, url) {
+                        return const ShimmerBox(
+                          height: double.infinity,
+                          width: double.infinity,
+                          borderRadius: 10.0,
+                          margin: 0.0,
+                        );
+                      },
+                      errorWidget: (context, url, error) {
+                        return Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                            color: Colors.grey.shade100,
                           ),
-                          color: Colors.grey.shade100,
-                        ),
-                        child: Icon(
-                          Icons.broken_image_rounded,
-                          color: Colors.grey.shade600,
-                          size: 35.0,
-                        ),
-                      );
-                    },
-                    fit: BoxFit.cover,
+                          child: Icon(
+                            Icons.broken_image_rounded,
+                            color: Colors.grey.shade600,
+                            size: 35.0,
+                          ),
+                        );
+                      },
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               );
