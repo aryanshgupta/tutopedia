d253cefdf7750b69b87a6769d01a657bf4ffb507
Aryansh Gupta <aryanshgupta@protonmail.com>
2023-03-16 10:52:14 +0530

    added cache network image

--- lib/components/course_slide_view.dart
+++ lib/components/course_slide_view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:tutopedia/constants/hive_boxes.dart';
 import 'package:tutopedia/models/course_model.dart';
@@ -47,11 +48,31 @@ class CourseSlideView extends StatelessWidget {
             child: Container(
               margin: const EdgeInsets.all(7.5),
               width: 200.0,
+              height: 112.5,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+                color: Colors.grey.shade100,
+              ),
               child: ClipRRect(
                 borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                child: Image.network(
-                  "https://i.ytimg.com/vi/${item.link.substring(30, 41)}/maxresdefault.jpg",
-                  fit: BoxFit.cover,
+                child: CachedNetworkImage(
+                  imageUrl: "https://i.ytimg.com/vi/${item.link.substring(30, 41)}/maxresdefault.jpg",
+                  placeholder: (context, url) {
+                    return Icon(
+                      Icons.image_rounded,
+                      color: Colors.grey.shade500,
+                      size: 35.0,
+                    );
+                  },
+                  errorWidget: (context, url, error) {
+                    return Icon(
+                      Icons.broken_image_rounded,
+                      color: Colors.grey.shade500,
+                      size: 35.0,
+                    );
+                  },
                 ),
               ),
             ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import package_info_plus
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
+import sqflite
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
